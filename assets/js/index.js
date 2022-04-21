936ef750ec2f7d6814a050bb6a0b0d165135c7e7
Kauã Landi <kaualandi@hotmail.com>
2022-04-21 17:50:30 -0300

    add history of searched icons

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,17 @@ const $input = document.querySelector('#input');
 const $output = document.querySelector('#output');
 const $preview = document.querySelector('#preview');
 const $iconState = document.querySelector('#icon-state');
+const $iconClearHistory = document.querySelector('#icon-clear-history');
 $iconState.style.display = 'none';
+$iconClearHistory.style.display = 'none';
+
+renderHistory();
 
 $inputForm.addEventListener('submit', (e) => {
     e.preventDefault();
     $preview.innerHTML = "";
-    $preview.innerHTML = $input.value;
+    const htmlTagIcon = $input.value;
+    $preview.innerHTML = htmlTagIcon;
     $output.innerHTML = "";
     $iconState.removeEventListener('click', copyToClipboard);
     $iconState.style.display = 'none';
@@ -16,6 +21,7 @@ $inputForm.addEventListener('submit', (e) => {
     $iconState.classList.remove('zoom-out');
     const checkSVG = setInterval(() => {
         if ($preview.innerHTML.includes('<svg')) {
+            saveHistory(htmlTagIcon);
             const regex = /<!--(.*)-->/g;
             const text = $preview.innerHTML.replace(regex, '');
             $output.innerHTML = text;
@@ -46,4 +52,54 @@ function copyToClipboard(text) {
             }, 2000);
         }, 1000);
     });
+}
+
+function saveHistory(htmlTagIcon) {
+    const history = localStorage.getItem('history');
+    if (history) {
+        const historyArray = JSON.parse(history);
+        if (historyArray.includes(htmlTagIcon)) {
+            return;
+        }
+        historyArray.push(htmlTagIcon);
+        localStorage.setItem('history', JSON.stringify(historyArray));
+    } else {
+        const historyArray = [];
+        historyArray.push(htmlTagIcon);
+        localStorage.setItem('history', JSON.stringify(historyArray));
+    }
+    renderHistory();
+}
+
+function renderHistory() {
+    const history = localStorage.getItem('history');
+    const $historyList = document.querySelector('#history-list');
+    $historyList.innerHTML = '';
+    if (history) {
+        const historyArray = JSON.parse(history).reverse();
+        if(historyArray.length > 0) {
+            historyArray.forEach((htmlTagIcon) => {
+                const $item = document.createElement('button');
+                $item.classList.add('history-item', 'nostyle');
+                $item.setAttribute('tag-origin', ''+htmlTagIcon);
+                $item.addEventListener('click', innerInputAndSubmit);
+                $item.innerHTML = htmlTagIcon;
+                $historyList.appendChild($item);
+            });
+            $iconClearHistory.style.display = 'inline-block';
+            $iconClearHistory.addEventListener('click', clearHistory);
+        } else $historyList.innerHTML = '<p class="text-center italic w-100">Seus ícones pesquisados aparecerão aqui.</p>';
+    } else $historyList.innerHTML = '<p class="text-center italic w-100">Seus ícones pesquisados aparecerão aqui.</p>';
+}
+
+function innerInputAndSubmit() {
+    const htmlTagIcon = this.getAttribute('tag-origin');
+    $input.value = htmlTagIcon;
+    $inputForm.querySelector('button[type="submit"]').click();
+}
+
+function clearHistory() {
+    localStorage.removeItem('history');
+    $iconClearHistory.style.display = 'none';
+    renderHistory();
 }
